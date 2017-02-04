7dfbe44667bbbde03fce2533e1c59c7cd4858615
pavolkuban <kuban@eub.sk>
2017-02-04 09:12:41 +0100

    Update App.java

--- src/main/java/io/thingface/examples/App.java
+++ src/main/java/io/thingface/examples/App.java
@@ -7,7 +7,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class App
 {
-    public static final String THINGFACE_HOST = "xyz.thingface.io";
+    public static final String THINGFACE_HOST = "dev-app.thingface.io";
     public static final int THINGFACE_PORT = 8883;
     public static final String THINGFACE_DEVICE_ID = "your_device_id";
     public static final String THINGFACE_DEVICE_SECRET = "your_device_secret";
